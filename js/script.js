e9136320aa6b54d10adb6ec1ca7e9cba90c7b17b
sohibjon1212 <xursanmurodovsohibjon@gmail.com>
2024-06-05 14:39:19 +0500

    Routing

--- js/script.js
+++ js/script.js
@@ -22,17 +22,80 @@ menuIcons[1].addEventListener("click", () => {
         isOpened = false;
     }
 })
-
-let liArray = document.querySelectorAll("li")
+ let paga = "home"
+let liArray = document.querySelectorAll("li");
 liArray.forEach(li => {
     li.onclick = function() {
 
         liArray.forEach(li => {
             li.classList.remove("active")
-
-
         })
-        li.classList.add("active")
+
+        li.classList.add("active");
+
+        console.log(li.id);
+
+        page = li.id;
+
+        switch(paga){
+            case "home":{
+                document.querySelector("#root").innerHTML =
+                `<h1>home bu</h1>
+              
+           
+              `
+            
+              ;
+                break;
+            }
+            case "short":{
+                document.querySelector("#root").innerHTML =
+                '<h1>short bu</h1>';
+                break;
+            }
+            case "subscription":{
+                document.querySelector("#root").innerHTML =
+                '<h1>subscription bu</h1>';
+                break;
+            }
+            case "channel":{
+                document.querySelector("#root").innerHTML =
+                '<h1>channel bu</h1>';
+                break;
+            }
+            case "history":{
+                document.querySelector("#root").innerHTML =
+                '<h1>history bu</h1>';
+                break;
+            }
+            case "playlists":{
+                document.querySelector("#root").innerHTML =
+                '<h1>playlists bu</h1>';
+                break;
+            }
+            case "Your videos":{
+                document.querySelector("#root").innerHTML =
+                '<h1>Your videos bu</h1>';
+                break;
+            }
+            case "watch later":{
+                document.querySelector("#root").innerHTML =
+                '<h1>watch later bu</h1>';
+                break;
+            }
+            case "like":{
+                document.querySelector("#root").innerHTML =
+                '<h1>like bu</h1>';
+                break;
+            }
+            default: {
+                document.querySelector("#root").innerHTML =
+                '<h1>404 not found</h1>'
+            }
+
+
+        }
+
 
     }
 
